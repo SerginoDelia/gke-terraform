d017070d77688b11dea0781930595c18052fe391
Sergino Delia <sergino.co@gmail.com>
2024-11-03 19:37:10 -0500

    added updates

--- 8-node-pools.tf
+++ 8-node-pools.tf
@@ -3,6 +3,16 @@ resource "google_service_account" "kubernetes" {
   account_id = "kubernetes"
 }
 
+# import the existing kubernetes service account
+# terraform import google_service_account.kubernetes projects/agwe-3/serviceAccounts/kubernetes@agwe-3.iam.gserviceaccount.com
+
+# pull the existing kubernetes service account
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+data "google_service_account" "kubernetes" {
+  account_id = "kubernetes"
+  project    = "agwe-3"
+}
+
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_node_pool
 resource "google_container_node_pool" "general" {
   name       = "general"
@@ -63,3 +73,15 @@ resource "google_container_node_pool" "spot" {
     ]
   }
 }
+
+# connect Kubernetes to local machine
+resource "null_resource" "get_credentials" {
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials primary --zone ${var.zone} --project ${var.project}"
+  }
+  depends_on = [google_container_cluster.primary]
+}
+
+
+
+
